1425b78f32151d2cb2dd6903ab3dd57dbd58e320
armanpiliposyan <94868094+Arman-Piliposyan@users.noreply.github.com>
2023-08-02 15:45:39 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('WORKIK');
+const widgetWrapper = document.createElement('div');
+widgetWrapper.classList.add('widget_wrapper');
+
+const widgetIcon = document.createElement('div');
+widgetIcon.classList.add('widget_icon');
+
+widgetWrapper.appendChild(widgetIcon);
+document.body.appendChild(widgetWrapper);
+
+widgetWrapper.addEventListener('click', () => {
+  console.log('ok');
+});
